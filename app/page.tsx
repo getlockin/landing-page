adedf1b51de67b61f191db19da7ccf332fe81d79
notcodesid <notcodesid@gmail.com>
2025-06-05 16:05:57 +0530

    features cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Appbar from "@/components/appbar";
+import Features from "@/components/featurs";
 import Hero from "@/components/hero";
 import VideoSection from "@/components/VideoSection"; // Import the new component
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col bg-black text-white">
+    <div>
       {/* Video element removed from here */}
       <Appbar />
       <Hero />
       <VideoSection /> {/* Add the new VideoSection component */}
+      <Features />
     </div>
   );
 }
--- components/VideoSection.tsx
+++ components/VideoSection.tsx
@@ -1,6 +1,6 @@
 export default function VideoSection() {
     return (
-      <div className="flex justify-center items-center mt-[50px]">
+      <div className="flex justify-center items-center my-[50px]">
         <video src="/lockin.mov"  width="1000" height="300" autoPlay controls loop muted={false} className="rounded-[5px]" />
       </div>
     )
